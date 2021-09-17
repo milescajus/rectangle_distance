b4c37eeba35e22858d7ba5effc79741f3460878a
milescajus <80205080+milescajus@users.noreply.github.com>
2021-09-17 01:02:47 -0500

    refactored code and added missing corner handling

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Rectangle Distance
 
 This repo contains a function to calculate the distance from a 2D point to the nearest edge of a rectangle, returning 0 if the point is contained within, or lies on the edge, of the rectangle.
+
+NB: be sure to compile with the `-lm` option to link to the C++ math library.
--- main.cpp
+++ main.cpp
@@ -12,74 +12,28 @@ struct Rect {
 float DistanceToRect(float x, float y, Rect& rect)
 {
 	// function returns the distance from a 2D point to the nearest edge of a rectangle
-	// returns 0 if the point is inside (or on the edge) of the rectangle
-	// two versions are included, with the more readable/explicit code commented out
+	// returns 0 if the point is inside of the rectangle
 	
-	// LESS READABLE
-	if (x <= rect.maxX) {
-		if (x < rect.minX) {
-			if (y < rect.minY) {
-				// Negative Diagonal
-				return sqrt(pow(abs(x), 2) + pow(abs(y), 2));
-			} else {
-				// Left
-				return abs(x);
-			}
-		} else if (y < rect.minY) {
-				// Above
-				return abs(y);
-			} else if (y > rect.maxY) {
-				// Below
-				return (y - rect.maxY);
-			} else {
-				// Inside Rect
-				return 0;
-
-			}
-	} else if (y <= rect.maxY) {
-			// Right
-			return (x - rect.maxX);
-	} else {
-			// Postive Diagonal
-			return sqrt(pow(x, 2) + pow(y, 2));
+	if (x >= rect.minX && x <= rect.maxX) {
+		if (y <= rect.minY) { return abs(y); } 			// Above
+		else if (y >= rect.maxY) { return (y - rect.maxY); } 	// Below
 	}
 
-	return 0;
-
-	/*
-	// MORE READABLE
-	// Below
-	if (x <= rect.maxX && y > rect.maxY) {
-		return (y - rect.maxY);
+	if (y >= rect.minY && y <= rect.maxY) {
+		if (x <= rect.minX) { return abs(x); } 			// Left
+		else if (x >= rect.maxX) { return (x - rect.maxX); } 	// Right
 	}
 
-	// Right
-	if (x > rect.maxX && y <= rect.maxY) {
-		return (x - rect.maxX);
-	}
-
-	// Postive Diagonal
-	if (x > rect.maxX && y > rect.maxY) {
-		return (sqrt(pow(x, 2) + pow(y, 2)));
-	}
-
-	// Above
-	if (x >= rect.minX && y < rect.minY) {
-		return abs(y);
-	}
-
-	// Left
-	if (x < rect.minX && y >= rect.minY) {
-		return abs(x);
-	}
-
-	// Negative Diagonal
-	if (x < rect.minX && y < rect.minY) {
+	if ((x < rect.minX && y < rect.minY)				// Top Left
+	    || (x > rect.maxX && y < rect.minY)				// Top Right
+	    || (x < rect.minX && y > rect.maxY)				// Bottom Left
+	    || (x > rect.maxX && y > rect.maxY)				// Bottom Right
+	   ) {
 		return (sqrt(pow(abs(x), 2) + pow(abs(y), 2)));
 	}
 
+	// Inside
 	return 0;
-	*/
 }
 
 int main(void)
@@ -91,11 +45,13 @@ int main(void)
 	r.maxY = 5;
 
 	// TEST CODE
-	printf("Below: %s\n", DistanceToRect(2, 7, r) == float(2) ? "Passed" : "Failed");
-	printf("Right: %s\n", DistanceToRect(7, 2, r) == float(2) ? "Passed" : "Failed");
-	printf("Postive Diagonal: %s\n", DistanceToRect(6, 8, r) == float(10) ? "Passed" : "Failed");
-	printf("Above: %s\n", DistanceToRect(2, -2, r) == float(2) ? "Passed" : "Failed");
-	printf("Left: %s\n", DistanceToRect(-2, 1, r) == float(2) ? "Passed" : "Failed");
-	printf("Negative Diagonal: %s\n", DistanceToRect(-3, -4, r) == float(5) ? "Passed" : "Failed");
-	printf("Inside Rect: %s\n", DistanceToRect(1, 2, r) == float(0) ? "Passed" : "Failed");
+	printf("Above: \t\t%s\n", DistanceToRect(2, -2, r) == float(2) ? "Passed" : "Failed");
+	printf("Below: \t\t%s\n", DistanceToRect(2, 7, r) == float(2) ? "Passed" : "Failed");
+	printf("Left: \t\t%s\n", DistanceToRect(-2, 1, r) == float(2) ? "Passed" : "Failed");
+	printf("Right: \t\t%s\n", DistanceToRect(7, 2, r) == float(2) ? "Passed" : "Failed");
+	printf("Top Left: \t%s\n", DistanceToRect(-6, -8, r) == float(10) ? "Passed" : "Failed");
+	printf("Top Right: \t%s\n", DistanceToRect(6, -8, r) == float(10) ? "Passed" : "Failed");
+	printf("Bottom Left: \t%s\n", DistanceToRect(-6, 8, r) == float(10) ? "Passed" : "Failed");
+	printf("Bottom Right: \t%s\n", DistanceToRect(6, 8, r) == float(10) ? "Passed" : "Failed");
+	printf("Inside Rect: \t%s\n", DistanceToRect(1, 2, r) == float(0) ? "Passed" : "Failed");
 }
